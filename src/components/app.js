aed47f513a4f1c1c9cc9d17212a36eed317a0feb
Autumn <autumncbullard@gmail.com>
2018-08-05 12:48:54 -0400

    Use Geolocation to get coordinates

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,31 @@ export default class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      coords: {
+        lat: null,
+        lng: null
+      }
+    };
+    this.getSuccess = this.getSuccess.bind(this);
+    this.getError = this.getError.bind(this);
+  }
+
+  getSuccess(position) {
+    this.setState({
+      coords: {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+    });
+  }
+
+  getError(err) {
+    console.log(err);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.getSuccess, this.getError);
   }
 
   render() {
