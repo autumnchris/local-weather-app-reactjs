a6e20af53ffa3a5a66b583256ba59c78e24e0722
Autumn <autumncbullard@gmail.com>
2018-08-05 12:36:55 -0400

    Add markup for footer

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,11 @@ export default class App extends Component {
         <header>
           <h1>View your local weather</h1>
         </header>
+        {/* FOOTER */}
+        <footer>
+          <span>Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></span>
+          <span>Powered by <a href="https://darksky.net/poweredby" target="_blank">Dark Sky</a></span>
+        </footer>
       </div>
     );
   }
