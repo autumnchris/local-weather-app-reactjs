cdc9944f8bf562e69ef35c2d9cdc92c17de0dd5e
Autumn <autumncbullard@gmail.com>
2018-08-13 18:43:22 -0400

    Update styling for error message

--- src/components/app.js
+++ src/components/app.js
@@ -145,7 +145,7 @@ export default class App extends Component {
             </div>
           </div>
           {/* ERROR MESSAGE */}
-          <p className="error-message" style={this.state.errorStyle}><span className="fa fa-exclamation-triangle fa-lg fa-fw"></span> {this.state.errorMessage}</p>
+          <p className="message error-message" style={this.state.errorStyle}><span className="fa fa-exclamation-circle fa-lg fa-fw"></span> {this.state.errorMessage}</p>
         </main>
         {/* FOOTER */}
         <footer>
