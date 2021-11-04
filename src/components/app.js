bb7dd4e938a1a51657f68080c482827282a7e90e
autumnchris <autumncbullard@gmail.com>
2021-11-04 13:54:05 -0400

    Update portfolio link

--- src/components/app.js
+++ src/components/app.js
@@ -64,7 +64,7 @@ const App = () => {
       <main>
         {loadingStatus ? <LoadingSpinner /> : <ResultsContainer weatherData={weatherData} errorMessage={errorMessage} />}
       </main>
-      <footer>Created by <a href="https://autumnbullard-portfolio.herokuapp.com" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
+      <footer>Created by <a href="https://autumnchris.github.io/portfolio" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
     </React.Fragment>
   );
 }
