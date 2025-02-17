0cb932cfb2b9dd21384786c5a2f18545ca017ab8
autumnchris <autumncbullard@gmail.com>
2025-02-17 10:18:44 -0500

    Improve semantic HTML

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,13 +15,17 @@ const Header = ({ pageLoadView, setModalVisibility, getGeolocation }) => {
         <div className="item">
           <h1>View the local weather</h1>
         </div>
-        <div className="item button-group">
-          <button type="button" className="button city-search-button" onClick={() => setModalVisibility(true)} aria-label="Search By City" title="Search By City">
-            <span className="fa-solid fa-magnifying-glass icon"></span>
-          </button>
-          <button type="button" className="button current-location-button" onClick={() => getGeolocation()} aria-label="Get Current Location" title="Get Current Location">
-            <span className="fa-solid fa-location-dot icon"></span>
-          </button>
+        <div className="item">
+          <aside>
+            <div className="button-group">
+              <button type="button" className="button city-search-button" onClick={() => setModalVisibility(true)} aria-label="Search By City" title="Search By City">
+                <span className="fa-solid fa-magnifying-glass icon"></span>
+              </button>
+              <button type="button" className="button current-location-button" onClick={() => getGeolocation()} aria-label="Get Current Location" title="Get Current Location">
+                <span className="fa-solid fa-location-dot icon"></span>
+              </button>
+            </div>
+          </aside>
         </div>
       </header>
     );
